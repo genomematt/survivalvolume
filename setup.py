2b14be6b304b8cbad5f7a1f0994076e3ff27c2b9
Matthew Wakefield <wakefield@wehi.edu.au>
2021-05-31 13:05:24 +1000

    update classifiers

--- setup.py
+++ setup.py
@@ -29,10 +29,10 @@ setup(
           'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
           'Operating System :: POSIX',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
 
